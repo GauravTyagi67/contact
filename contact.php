053f94cc3ded7192dc06b61d0042e47d8b70f85a
Gaurav Tyagi <62422447+GauravTyagi67@users.noreply.github.com>
2021-03-14 13:00:10 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+
+	if(isset($_POST['submit']))
+	{
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$msg=$_POST['msg'];
+		$to='xyz-email@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers))
+		{
+			echo "<h3>Sent Successfully! Thank you"." <span style='color:red;'>".$name."</span>, We will contact you shortly!</h3>". "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+		}
+		else
+		{
+			echo "Something went wrong!";
+		}
+	}
+
+?>
